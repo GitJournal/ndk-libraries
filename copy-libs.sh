179693ca3d9c88ef5b6a036ece3fad1e908683a2
Vishesh Handa <me@vhanda.in>
2019-05-13 13:31:23 +0200

    Add a script to copy the libs to the correct location

---
+++ copy-libs.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eux
+
+BASE_DIR=$1
+
+if [ ! -d $BASE_DIR/android/app/libs ]; then
+    echo "Libs directory does not exist"
+    exit 1
+fi
+
+cp -r libs/libgit2/* $BASE_DIR/android/app/libs/
+cp -r libs/libssh2/* $BASE_DIR/android/app/libs/
+cp -r libs/openssl-lib/* $BASE_DIR/android/app/libs/
