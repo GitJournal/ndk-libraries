a50772fc174e885147298bcab9ac2a1b7ef323e1
Vishesh Handa <me@vhanda.in>
2019-05-13 16:26:15 +0200

    build_all: Cleanup afterwords
    The cleanup script is not perfect, but it does help a bit.

--- build_all.sh
+++ build_all.sh
@@ -2,6 +2,14 @@
 
 set -eux
 
+start=`date +%s`
+
 ./build-openssl-android.sh
 ./build-ssh2.sh
 ./build-git2.sh
+./cleanup.sh
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo "Script took $runtime seconds"
