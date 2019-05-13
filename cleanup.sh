764d1f0f184581d9f572bed6d22fe4521fc13b67
Vishesh Handa <me@vhanda.in>
2019-05-13 16:31:15 +0200

    Fix cleanup script
    It's not perfect, but it works

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env sh
 
-set -eux
+set -u
 
 cd libs
+find . -type d -name 'misc' -exec rm -rf {} \;
+find . -type d -name 'bin' -exec rm -rf {} \;
+find . -type d -name 'certs' -exec rm -rf {} \;
+find . -type d -name 'share' -exec rm -rf {} \;
+find . -type d -name 'private' -exec rm -rf {} \;
 find . -type f -not -regex '.*\.[ah]' -exec rm {} \;
 find . -type d -empty -delete
