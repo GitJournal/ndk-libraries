55ff91bab3a8cc0ca625ecc5850f289384603671
Vishesh Handa <me@vhanda.in>
2019-05-14 12:58:55 +0200

    Update copy-libs

--- copy-libs.sh
+++ copy-libs.sh
@@ -10,5 +10,6 @@ if [ ! -d $BASE_DIR/android/app/libs ]; then
 fi
 
 cp -r libs/libgit2/* $BASE_DIR/android/app/libs/
+cp -r libs/libssh/* $BASE_DIR/android/app/libs/
 cp -r libs/libssh2/* $BASE_DIR/android/app/libs/
 cp -r libs/openssl-lib/* $BASE_DIR/android/app/libs/
